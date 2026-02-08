83e5f7166936fc94ba55c97cdc4b910aeba115f9
JiangYingEr <kongdezhang123@foxmail.com>
2026-02-08 14:34:15 +0800

    Update run_defense.sh

--- Experiment/Stealthiness/SISTAR/run_defense.sh
+++ Experiment/Stealthiness/SISTAR/run_defense.sh
@@ -3,8 +3,16 @@
 echo ">>> Start configuring S1 defense..."
 
 echo "Setting Thresholds to 50 PPS..."
-simple_switch_CLI --thrift-port 9090 <<< "register_write MyIngress.threshold_register32 7 50"
-simple_switch_CLI --thrift-port 9090 <<< "register_write MyIngress.threshold_register32 8 50"
+simple_switch_CLI --thrift-port 9090 <<< "register_write MyIngress.threshold_register32 0 6"
+simple_switch_CLI --thrift-port 9090 <<< "register_write MyIngress.threshold_register32 1 660"
+simple_switch_CLI --thrift-port 9090 <<< "register_write MyIngress.threshold_register32 2 660"
+simple_switch_CLI --thrift-port 9090 <<< "register_write MyIngress.threshold_register32 3 1"
+simple_switch_CLI --thrift-port 9090 <<< "register_write MyIngress.threshold_register32 4 1"
+simple_switch_CLI --thrift-port 9090 <<< "register_write MyIngress.threshold_register32 5 1"
+simple_switch_CLI --thrift-port 9090 <<< "register_write MyIngress.threshold_register32 6 426"
+simple_switch_CLI --thrift-port 9090 <<< "register_write MyIngress.threshold_register32 7 81312"
+simple_switch_CLI --thrift-port 9090 <<< "register_write MyIngress.threshold_register32 8 81312"
+
 
 simple_switch_CLI --thrift-port 9090 <<< "table_clear MyIngress.DDoS_ternary"
 
